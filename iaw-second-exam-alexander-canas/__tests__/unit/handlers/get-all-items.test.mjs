9052f0d8859e0876536f6269342cd4656bf89c4a
Nifni-alex <alexandercanas1505@gmail.com>
2024-02-26 13:34:49 +0100

    version3

--- iaw-second-exam-alexander-canas/__tests__/unit/handlers/get-all-items.test.mjs
+++ iaw-second-exam-alexander-canas/__tests__/unit/handlers/get-all-items.test.mjs
@@ -21,7 +21,7 @@ describe('Test getAllItemsHandler', () => {
         }); 
  
         const event = { 
-            httpMethod: 'PUT' 
+            httpMethod: 'POST' 
         };
  
         // Invoke helloFromLambdaHandler() 
